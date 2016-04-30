bb5f765036d390ca97edfac81fd7a26588dc8837
Krzysztof <k.jerzynski1@gmail.com>
2016-04-30 16:54:25 +0200

    + adding module backup for manager shop

---
+++ module_backup/backup.java
@@ -0,0 +1,62 @@
+package test_backup;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
+
+public class backup {
+
+	 public static void main(String[] a) throws Exception {
+		 
+		 DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd_HH-mm-ss");
+		 Date date = new Date(); 
+		    zipFolder("C:\\xampp2\\htdocs\\sklep", "C:\\xampp2\\backupFolder\\backup_"+ dateFormat.format(date).toString() + ".zip");
+	 }
+
+		  static public void zipFolder(String srcFolder, String destZipFile) throws Exception {
+		    ZipOutputStream zip = null;
+		    FileOutputStream fileWriter = null;
+
+		    fileWriter = new FileOutputStream(destZipFile);
+		    zip = new ZipOutputStream(fileWriter);
+
+		    addFolderToZip("", srcFolder, zip);
+		    zip.flush();
+		    zip.close();
+		  }
+
+		  static private void addFileToZip(String path, String srcFile, ZipOutputStream zip)
+		      throws Exception {
+
+		    File folder = new File(srcFile);
+		    if (folder.isDirectory()) {
+		      addFolderToZip(path, srcFile, zip);
+		    } else {
+		      byte[] buf = new byte[1024];
+		      int len;
+		      FileInputStream in = new FileInputStream(srcFile);
+		      zip.putNextEntry(new ZipEntry(path + "/" + folder.getName()));
+		      while ((len = in.read(buf)) > 0) {
+		        zip.write(buf, 0, len);
+		      }
+		    }
+		  }
+
+		  static private void addFolderToZip(String path, String srcFolder, ZipOutputStream zip)
+		      throws Exception {
+		    File folder = new File(srcFolder);
+
+		    for (String fileName : folder.list()) {
+		      if (path.equals("")) {
+		        addFileToZip(folder.getName(), srcFolder + "/" + fileName, zip);
+		      } else {
+		        addFileToZip(path + "/" + folder.getName(), srcFolder + "/" + fileName, zip);
+		      }
+		    }
+		  }
+}
